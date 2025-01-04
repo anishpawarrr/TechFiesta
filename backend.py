900b7b1db9143a3d570c8b96a80245d7f466b109
anishpawarrr <anishpurupawar@gmail.com>
2025-01-04 23:28:59 +0530

    prototype

---
+++ backend.py
@@ -0,0 +1,46 @@
+from huggingface_hub import InferenceClient
+from env import *
+client = InferenceClient(api_key=key)
+
+def ask(image_url, user_query = ""):
+    messages = [{"role" : "system", "content" : [{"type":"text", "text":sys_prompt}]},
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": user_prompt + user_query  
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": image_url
+                    }
+                }
+            ]
+        }
+    ]
+
+    completion = client.chat.completions.create(
+        model=model, 
+        messages=messages, 
+        max_tokens=500
+    )
+
+    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
+
+    stream = client.chat.completions.create(
+        model="meta-llama/Llama-3.2-11B-Vision-Instruct", 
+        messages=messages, 
+        max_tokens=500,
+        stream=True
+    )
+
+    for chunk in stream:
+        print(chunk.choices[0].delta.content, end="")
+    
+
+# image_url = "https://media.istockphoto.com/id/471457370/photo/x-ray-image-of-broken-forearm-ap-and-lateral-view.jpg?s=612x612&w=0&k=20&c=x9K8ITR-C7FI70xMoCOS2zEi1915bgdj0HYVyZbM81g="
+
+# ask(image_url)
---
+++ frontend.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
+from backend import ask
+
+def load_image(image_url):
+    try:
+        response = requests.get(image_url)
+        img = Image.open(BytesIO(response.content))
+        return img
+    except Exception as e:
+        st.error(f"Error loading image: {e}")
+        return None
+
+def generate_llm_response(input_text):
+    # write actual code to generate a response
+    return f"LLM response to your input: {input_text}"
+
+st.title("AI-Powered Doctor Assistant")
+image_url = st.text_input("Enter image URL:")
+
+if image_url:
+    img = load_image(image_url)
+    if img:
+        fixed_width = 200
+        st.image(img,width=fixed_width)
+
+    user_input = st.text_input("Ask something related to the image:")
+
+    submit_button = st.button("Ask V-LLM")
+
+    if submit_button:
+        llm_response = ask(image_url, user_input)
+        st.write(llm_response)
+
+    # if user_input:
+    #     llm_response = generate_llm_response(user_input)
+    #     st.write(llm_response)
